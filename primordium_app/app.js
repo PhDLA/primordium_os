e84abd6649dfc56264e29691bf56f9dbb7f21ce7
ATTILA ZSORI <zsoriattila@hotmail.com>
2026-02-15 00:50:41 +0100

    Update app.js

--- primordium_app/app.js
+++ primordium_app/app.js
@@ -2,13 +2,15 @@ const core = require("./core/app_core.js");
 const runtime = require("./runtime/app_runtime.js");
 const ui = require("./ui/app_ui.js");
 
-(async () => {
-    console.log("=== PRIMORDIUM APPLICATION START ===");
+module.exports = {
+    async start() {
+        console.log("=== PRIMORDIUM APPLICATION START ===");
 
-    core.init();
-    ui.start();
+        core.init();
+        ui.start();
 
-    await runtime.run("hello primordium");
+        await runtime.run("hello primordium");
 
-    console.log("=== PRIMORDIUM APPLICATION END ===");
-})();
+        console.log("=== PRIMORDIUM APPLICATION END ===");
+    }
+};
