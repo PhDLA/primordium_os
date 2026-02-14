d44c02aab23d10f67abdd1bac4106fbc9ae7dce3
ATTILA ZSORI <zsoriattila@hotmail.com>
2026-02-15 00:29:07 +0100

    Update app.js

--- primordium_web/client/app.js
+++ primordium_web/client/app.js
@@ -1,5 +1,5 @@
 const logBox = document.getElementById("log");
-const ws = new WebSocket("ws://localhost:8080");
+const ws = new WebSocket(`wss://${location.host}`);
 
 ws.onmessage = (event) => {
     logBox.textContent += event.data + "\n";
