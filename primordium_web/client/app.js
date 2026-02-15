f7a422969a5a91ac7a4ce67745b91d4b202cafb5
ATTILA ZSORI <zsoriattila@hotmail.com>
2026-02-15 03:02:31 +0100

    Update app.js

--- primordium_web/client/app.js
+++ primordium_web/client/app.js
@@ -6,33 +6,3 @@ ws.onmessage = (event) => {
     logBox.scrollTop = logBox.scrollHeight;
 };
 
-socket.onmessage = (event) => {
-    const text = event.data;
-    const logBox = document.getElementById("logBox");
-
-    // PROGRESS BAR
-    if (text.startsWith("__PROGRESS__")) {
-        const clean = text.replace("__PROGRESS__", "").trim();
-
-        // jelenlegi sorok
-        let lines = logBox.textContent.split("\n");
-
-        // ha az utolsó sor progress bar → felülírjuk
-        if (lines[lines.length - 1].startsWith("[MODULE] Loading")) {
-            lines[lines.length - 1] = clean;
-        } else {
-            lines.push(clean);
-        }
-
-        // teljes tartalom újraépítése
-        logBox.textContent = lines.join("\n");
-        return;
-    }
-
-    // NORMÁL SOR
-    let lines = logBox.textContent.split("\n");
-    lines.push(text);
-    logBox.textContent = lines.join("\n");
-};
-
-
